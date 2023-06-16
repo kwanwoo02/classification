42caf5ba6ccaaba1a6006b3fa907ca71498eb766
kwanwoo02 <tom842088@gmail.com>
2023-06-16 14:27:20 +0900

    sourcode update

--- PL_BinaryCrossentropy.py
+++ PL_BinaryCrossentropy.py
@@ -1,1 +1,117 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+from torchvision import transforms
+import torch.optim as optim
+import pytorch_lightning as pl
+import timm
+from torchvision import datasets, models, transforms
+import os
+from torch.optim import lr_scheduler
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning import Trainer
+import wandb
+from torchmetrics.classification import BinaryAccuracy
+from pytorch_lightning.callbacks import EarlyStopping,ModelCheckpoint
+import argparse
+import yaml
 
+
+class LitAutoEncoder(pl.LightningModule):
+    def __init__(self,model_name, threshold, lr =2e-4):
+        super().__init__()
+        self.lr = lr
+        self.backbone = timm.create_model(model_name, pretrained=True, num_classes=1)
+        self.criterion = nn.BCEWithLogitsLoss()
+        self.accuracy = BinaryAccuracy(threshold=threshold)
+    def forward(self, x):
+        outputs = self.backbone(x)
+        return outputs
+
+    def training_step(self, batch, batch_idx):
+        inputs, labels = batch
+        outputs = self(inputs)
+        loss = self.criterion(outputs, labels.unsqueeze(1).float())
+        acc = self.accuracy(outputs, labels.unsqueeze(1).float())
+        
+        self.log('train_loss', loss ,on_step = False, on_epoch = True, logger = True);
+        self.log('train_acc', acc ,on_step = False, on_epoch = True, logger = True);
+        
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        inputs, labels = batch
+        outputs = self(inputs)
+        loss = self.criterion(outputs, labels.unsqueeze(1).float())
+        acc = self.accuracy(outputs, labels.unsqueeze(1).float())
+        self.log('val_loss', loss ,on_step = False, on_epoch = True, logger = True);
+        self.log('val_acc', acc ,on_step = False, on_epoch = True, logger = True);
+        
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr = self.lr)
+        return optimizer
+
+    
+def run(args):
+    
+    argstr = yaml.dump(args.__dict__, default_flow_style = False)
+    print(f"\nTraining Arguments:\n\n{argstr}")
+    
+    wandb.login(key='6e1fe556323d6ff4de8ebd2b657707a17715f7e8')
+    wandb_logger = WandbLogger(project='PETDIS_Binary',# group runs in "MNIST" project
+                           name=f"{args.model_name}_{os.path.basename(args.root)}_{args.batch_size}_{args.learning_rate}",
+                           log_model='all') 
+    # Data transformations
+    transform = {
+        'train':transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.48772067, 0.45476404, 0.41735706), (0.22629902, 0.22098567, 0.22134961))]),
+        'val':transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.49372694, 0.45860133, 0.41793576), (0.22488698, 0.22060247, 0.22134696))
+        ]),
+    }
+    image_datasets = {x: datasets.ImageFolder(os.path.join(args.root, x),
+                                              transform[x])
+                      for x in ['train', 'val']}
+
+    # Create data loaders
+    train_loader = DataLoader(image_datasets['train'], batch_size=args.batch_size, shuffle=True)
+    val_loader = DataLoader(image_datasets['val'], batch_size=args.batch_size)
+    # Create the model
+    model = LitAutoEncoder(args.model_name,args.Threshold)
+    # Create a PyTorch Lightning trainer
+    trainer = Trainer(logger=wandb_logger, devices=args.devices, max_epochs= args.epochs,
+                     callbacks=[EarlyStopping(monitor="val_loss", mode="min"),ModelCheckpoint(dirpath = args.save_model_path, filename = f"{args.model_name}_best")] )
+
+    # Train the model
+    trainer.fit(model, train_loader, val_loader)
+    wandb.finish()
+    
+if __name__ == "__main__":
+    
+    # Initialize Argument Parser    
+    parser = argparse.ArgumentParser(description = 'Image Classification Training Arguments')
+    
+    # Add arguments to the parser
+    parser.add_argument("-r", "--root", type = str, default = '/home/ubuntu/workspace/kwanwoo/PETDIS/dataset/cat&dog', help = "Path to the data")
+    parser.add_argument("-bs", "--batch_size", type = int, default = 128, help = "Mini-batch size")
+    parser.add_argument("-is", "--inp_im_size", type = tuple, default = (224, 224), help = "Input image size")
+    parser.add_argument("-mn", "--model_name", type = str, default = 'resnet34.a1_in1k', help = "Model name for backbone")
+    parser.add_argument("-d", "--devices", type = int, default = 3, help = "Number of GPUs for training")
+    parser.add_argument("-lr", "--learning_rate", type = float, default = 3e-5, help = "Learning rate value")
+    parser.add_argument("-T", "--Threshold", type = float, default = 0.5, help = "Binary T classification Threshold")
+    parser.add_argument("-e", "--epochs", type = int, default = 20, help = "Train epochs number")
+    parser.add_argument("-sm", "--save_model_path", type = str, default = 'saved_models', help = "Path to the directory to save a trained model")
+    parser.add_argument("-sr", "--save_results_path", type = str, default = 'results', help = "Path to the directory to save the train results")
+    
+    # Parse the added arguments
+    args = parser.parse_args() 
+    
+    # Run the script with the parsed arguments
+    run(args)
--- PL_Crossentropy.py
+++ PL_Crossentropy.py
@@ -1,252 +1,98 @@
-import torch, torchmetrics, wandb, timm, argparse, yaml
-import pytorch_lightning as pl
-from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-from pytorch_lightning.callbacks import ModelCheckpoint, Callback
+import torch
 from torch import nn
-from torch.nn import functional as F
-from torch.utils.data import random_split, DataLoader
-from torchvision import transforms as tfs
-from torchvision.datasets import ImageFolder, CIFAR100, CIFAR10
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+from torchvision import transforms
+import torch.optim as optim
+import pytorch_lightning as pl
+import timm
+from torchvision import datasets, models, transforms
+import os
+from torch.optim import lr_scheduler
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning import Trainer
+import wandb
+import torchmetrics
+from pytorch_lightning.callbacks import EarlyStopping,ModelCheckpoint
+import argparse
+import yaml
 
-class CustomDataset(pl.LightningDataModule):
-    
-    """
-    
-    This class gets several arguments and returns train, validation, and test dataloaders.
-    
-    Parameters:
-    
-        root      - path to the directory with images, str;
-        bs        - mini batch size, int;
-        im_dims   - input image dimensions, tuple -> int.
-        
-    Outputs:
-    
-        tr_dl     - train dataloader, torch dataloader object;
-        val_dl    - validation dataloader, torch dataloader object;
-        test_dl   - test dataloader, torch dataloader object;
-    
-    """
-    
-    def __init__(self, root, bs, im_dims = (224, 224)):
-        
+
+class LitAutoEncoder(pl.LightningModule):
+    def __init__(self,model_name, num_classes, lr =2e-4):
         super().__init__()
-        self.root, self.bs = root, bs
-
-        # Initialize transformations
-        self.transform = tfs.Compose([
-                         # Resize image dimensions
-                         tfs.Resize((im_dims)),
-                         # Transform grayscale (1 channel) images into 3 channels
-                         tfs.Grayscale(num_output_channels = 3),
-                         # Transform PIL images into tensor objects
-                         tfs.ToTensor()]
-                         )
-    
-    def check(self, path): 
-        
-        """
-        
-        This function gets an image path and checks wheter it is a valid image file or not.
-        
-        Parameter:
-        
-            path  - an image path, str.
-            
-        Output:
-        
-            valid - whether or not the path is a valid image, bool.
-        
-        """
-        
-        # Initialize a list with valid image types
-        valid_types = [".png", ".jpg", ".jpeg"]
-        for valid_type in valid_types:
-            if valid_type in path.lower():
-                return True
-        return False
-    
-    def data_setup(self):
-        
-        """
-        
-        This function reads images from folder, splits the data and returns class names and number of classes in the dataset.
-        
-        Outputs:
-            
-            cls_names    - name of classes in the dataset, list;
-            num_classes  - number of classes in the dataset, int.
-        
-        """
-        
-        self.ds = ImageFolder(root = self.root, transform = self.transform, is_valid_file = self.check)
-
-        # Get class names
-        cls_names = list(self.ds.class_to_idx.keys())
-        
-        # Get number of classes
-        num_classes = len(cls_names)
-        
-        # Get length of the dataset
-        ds_len = len(self.ds)
-        
-        # Get length for train and validation datasets
-        tr_len, val_len = int(ds_len * 0.8), int(ds_len * 0.1) 
-        
-        # Split the dataset into train, validation, and test datasets
-        self.tr_ds, self.val_ds, self.test_ds = random_split(self.ds, [tr_len, val_len, ds_len - (tr_len + val_len)])
-        
-        print(f"Number of train set images: {len(self.tr_ds)}")
-        print(f"Number of validation set images: {len(self.val_ds)}")
-        print(f"Number of test set images: {len(self.test_ds)}\n")
-        
-        tr_dl = DataLoader(self.tr_ds, batch_size = self.bs, shuffle = True)
-        val_dl = DataLoader(self.val_ds, batch_size = self.bs, shuffle = False)
-        test_dl = DataLoader(self.test_ds, batch_size = self.bs, shuffle = False)
-        
-        return tr_dl, val_dl, test_dl, cls_names, num_classes
-
-    
-class LitModel(pl.LightningModule):
-    
-    """"
-    
-    This class gets several arguments and returns a model for training.
-    
-    Parameters:
-    
-        input_shape  - shape of input to the model, tuple -> int;
-        model_name   - name of the model from timm library, str;
-        num_classes  - number of classes to be outputed from the model, int;
-        lr           - learning rate value, float.
-    
-    """
-    
-    def __init__(self, input_shape, model_name, num_classes, lr = 2e-4):
-        super().__init__()
-        
-        # log hyperparameters
-        self.save_hyperparameters()
         self.lr = lr
-        self.accuracy = torchmetrics.Accuracy(task = "multiclass", num_classes = num_classes)
-        self.model = timm.create_model(model_name, pretrained = True, num_classes = num_classes)
+        self.backbone = timm.create_model(model_name, pretrained=True, num_classes=num_classes)
+        self.criterion = nn.CrossEntropyLoss()
+        self.accuracy = torchmetrics.Accuracy('multiclass',num_classes=num_classes)
+        
+    def forward(self, x):
+        outputs = self.backbone(x)
+        return outputs
 
-    def configure_optimizers(self): return torch.optim.Adam(self.parameters(), lr = self.lr)
-    
-    def forward(self, x): return self.model(x)
-    
     def training_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self(x)
-        loss = F.cross_entropy(logits, y)
+        inputs, labels = batch
+        outputs = self(inputs)
+        loss = self.criterion(outputs, labels)
+        acc = self.accuracy(outputs, labels)
         
-        # Train metrics
-        preds = torch.argmax(logits, dim = 1)
-        acc = self.accuracy(preds, y)
-        self.log('train_loss', loss, on_step = False, on_epoch = True, logger = True)
-        self.log('train_acc', acc, on_step = False, on_epoch = True, logger = True)
+        self.log('train_loss', loss ,on_step = False, on_epoch = True, logger = True);
+        self.log('train_acc', acc ,on_step = False, on_epoch = True, logger = True);
         
         return loss
-    
+
     def validation_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self(x)
-        loss = F.cross_entropy(logits, y)
-
-        # Validation metrics
-        preds = torch.argmax(logits, dim = 1)
-        acc = self.accuracy(preds, y)
-        self.log('val_loss', loss, prog_bar = True)
-        self.log('val_acc', acc, prog_bar = True)
-        
-        return loss
-    
-    def test_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self(x)
-        loss = F.cross_entropy(logits, y)
-        
-        # Test metrics
-        preds = torch.argmax(logits, dim = 1)
-        acc = self.accuracy(preds, y)
-        self.log('test_loss', loss, prog_bar = True)
-        self.log('test_acc', acc, prog_bar = True)
+        inputs, labels = batch
+        outputs = self(inputs)
+        loss = self.criterion(outputs, labels)
+        acc = self.accuracy(outputs, labels)
+        self.log('val_loss', loss ,on_step = False, on_epoch = True, logger = True);
+        self.log('val_acc', acc ,on_step = False, on_epoch = True, logger = True);
         
         return loss
 
-class ImagePredictionLogger(Callback):
-    
-    def __init__(self, val_samples, cls_names = None, num_samples = 4):
-        super().__init__()
-        self.num_samples, self.cls_names = num_samples, cls_names
-        self.val_imgs, self.val_labels = val_samples
-        
-    def on_validation_epoch_end(self, trainer, pl_module):
-        
-        # Bring the tensors to CPU
-        val_imgs = self.val_imgs.to(device = pl_module.device)
-        val_labels = self.val_labels.to(device = pl_module.device)
-        # Get model prediction
-        logits = pl_module(val_imgs)
-        preds = torch.argmax(logits, -1)
-        
-        # Log the images as wandb Image
-        if self.cls_names != None:
-            trainer.logger.experiment.log({
-                "Sample Validation Prediction Results":[wandb.Image(x, caption = f"Predicted class: {self.cls_names[pred]}, Ground truth class: {self.cls_names[y]}") 
-                               for x, pred, y in zip(val_imgs[:self.num_samples], 
-                                                     preds[:self.num_samples], 
-                                                     val_labels[:self.num_samples])]})
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr = self.lr)
+        return optimizer
 
-        
+    
 def run(args):
     
-    """
-    
-    This function runs the main script based on the arguments.
-    
-    Parameter:
-    
-        args - parsed arguments.
-        
-    Output:
-    
-        train process.
-    
-    """
-    
-    # Get train arguments    
     argstr = yaml.dump(args.__dict__, default_flow_style = False)
     print(f"\nTraining Arguments:\n\n{argstr}")
     
-    if args.dataset_name == "custom":
-        dm = CustomDataset(args.root, bs = args.batch_size, im_dims = args.inp_im_size)
-        tr_dl, val_dl, test_dl, cls_names, num_classes = dm.data_setup()
-    elif args.dataset_name == "cifar10":
-        dm = CIFAR10DataModule(64)
-        tr_dl, val_dl, test_dl, cls_names, num_classes = dm.data_setup()
+    wandb.login(key='6e1fe556323d6ff4de8ebd2b657707a17715f7e8')
+    wandb_logger = WandbLogger(project='PETDIS',# group runs in "MNIST" project
+                           name=f"{args.model_name}_{os.path.basename(args.root)}_{args.batch_size}_{args.learning_rate}",
+                           log_model='all') 
+    # Data transformations
+    transform = {
+        'train':transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.48772067, 0.45476404, 0.41735706), (0.22629902, 0.22098567, 0.22134961))]),
+        'val':transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.49372694, 0.45860133, 0.41793576), (0.22488698, 0.22060247, 0.22134696))
+        ]),
+    }
+    image_datasets = {x: datasets.ImageFolder(os.path.join(args.root, x),
+                                              transform[x])
+                      for x in ['train', 'val']}
 
-    # Samples required by the custom ImagePredictionLogger callback to log image predictions.
-    val_samples = next(iter(val_dl))
-    val_imgs, val_labels = val_samples[0], val_samples[1]
-
-    # model = LitModel(args.inp_im_size, args.model_name, num_classes) if args.dataset_name == 'custom' else LitModel((32, 32), args.model_name, num_classes)
-    model = LitModel(args.inp_im_size, args.model_name, num_classes) 
-
-    # Initialize wandb logger
-    wandb_logger = WandbLogger(project = "classification", job_type = "train", name = f"{args.model_name}_{args.dataset_name}_{args.batch_size}_{args.learning_rate}")
-
-    # Initialize a trainer
-    trainer = pl.Trainer(max_epochs = args.epochs, gpus = args.devices, accelerator="gpu", devices = args.devices, strategy = "ddp", logger = wandb_logger,
-                         callbacks = [EarlyStopping(monitor = "val_loss", mode = "min"), ImagePredictionLogger(val_samples, cls_names),
-                                      ModelCheckpoint(monitor = "val_loss", dirpath = args.save_model_path, filename = f"{args.model_name}_best")])
+    # Create data loaders
+    train_loader = DataLoader(image_datasets['train'], batch_size=args.batch_size, shuffle=True)
+    val_loader = DataLoader(image_datasets['val'], batch_size=args.batch_size)
+    num_classes = len(image_datasets['train'].classes)
+    # Create the model
+    model = LitAutoEncoder(args.model_name,num_classes)
+    # Create a PyTorch Lightning trainer
+    trainer = Trainer(logger=wandb_logger, devices=args.devices, max_epochs= args.epochs,
+                     callbacks=[EarlyStopping(monitor="val_loss", mode="min"),ModelCheckpoint(dirpath = args.save_model_path, filename = f"{args.model_name}_best")] )
 
     # Train the model
-    trainer.fit(model, tr_dl, val_dl)
-    # Test the model
-    trainer.test(dataloaders = test_dl)
-    # Close wandb run
+    trainer.fit(model, train_loader, val_loader)
     wandb.finish()
     
 if __name__ == "__main__":
@@ -255,13 +101,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'Image Classification Training Arguments')
     
     # Add arguments to the parser
-    parser.add_argument("-r", "--root", type = str, default = 'path/to/your/data', help = "Path to the data")
-    parser.add_argument("-bs", "--batch_size", type = int, default = 64, help = "Mini-batch size")
+    parser.add_argument("-r", "--root", type = str, default = 'dataset/cat&dog', help = "Path to the data")
+    parser.add_argument("-bs", "--batch_size", type = int, default = 128, help = "Mini-batch size")
     parser.add_argument("-is", "--inp_im_size", type = tuple, default = (224, 224), help = "Input image size")
-    parser.add_argument("-dn", "--dataset_name", type = str, default = 'custom', help = "Dataset name for training")
-    parser.add_argument("-mn", "--model_name", type = str, default = 'rexnet_150', help = "Model name for backbone")
-    # parser.add_argument("-mn", "--model_name", type = str, default = 'vit_base_patch16_224', help = "Model name for backbone")
-    # parser.add_argument("-mn", "--model_name", type = str, default = 'vgg16_bn', help = "Model name for backbone")
+    parser.add_argument("-mn", "--model_name", type = str, default = 'resnet34.a1_in1k', help = "Model name for backbone")
     parser.add_argument("-d", "--devices", type = int, default = 3, help = "Number of GPUs for training")
     parser.add_argument("-lr", "--learning_rate", type = float, default = 3e-5, help = "Learning rate value")
     parser.add_argument("-e", "--epochs", type = int, default = 20, help = "Train epochs number")
